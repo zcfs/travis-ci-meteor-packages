75838e1119cc68ced33493f8ca19f95025c97a3a
Morten N.O. Nørgaard Henriksen <mh@gi2.dk>
2014-03-22 22:01:41 +0100

    Update start_test.js

--- start_test.js
+++ start_test.js
@@ -1,5 +1,6 @@
 #!/usr/bin/env node
 var spawn = require('child_process').spawn;
+var exec = require('child_process').exec;
 
 var workingDir = process.env.WORKING_DIR || process.env.PACKAGE_DIR || './';
 var args = ['test-packages', '--once', '--driver-package', 'test-in-console', '-p', 10015];
@@ -9,21 +10,34 @@ if (typeof process.env.PACKAGES === 'undefined') {
 else if (process.env.PACKAGES !== '') {
   args = args.concat(process.env.PACKAGES.split(';'));
 }
-var meteor = spawn('mrt', args, {cwd: workingDir});
-meteor.stdout.pipe(process.stdout);
-meteor.stderr.pipe(process.stderr);
-meteor.on('close', function (code) {
-  console.log('mrt exited with code ' + code);
-  process.exit(code);
-});
 
-meteor.stdout.on('data', function startTesting(data) {
-  var data = data.toString();
-  if(data.match(/10015|test-in-console listening/)) {
-    console.log('starting testing...');
-    meteor.stdout.removeListener('data', startTesting);
-    runTestSuite();
-  } 
+var meteor;
+
+exec('mrt update --force', {cwd: workingDir}, function(err, stdout, stderr) {
+  if (err) {
+    console.log('could not mrt update, Error: ' + err);
+  } else {
+    ///
+   
+    meteor = spawn('mrt', args, {cwd: workingDir});
+    meteor.stdout.pipe(process.stdout);
+    meteor.stderr.pipe(process.stderr);
+    meteor.on('close', function (code) {
+      console.log('mrt exited with code ' + code);
+      process.exit(code);
+    });
+    
+    meteor.stdout.on('data', function startTesting(data) {
+      var data = data.toString();
+      if(data.match(/10015|test-in-console listening/)) {
+        console.log('starting testing...');
+        meteor.stdout.removeListener('data', startTesting);
+        runTestSuite();
+      } 
+    });    
+    
+    ///
+  }
 });
 
 function runTestSuite() {
